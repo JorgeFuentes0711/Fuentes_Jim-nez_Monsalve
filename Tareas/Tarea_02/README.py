cc6e2336435d2e208b4c3ea1ddf6593fea775ab0
JorgeFuentes0711 <jorgefuentes0711@gmail.com>
2023-09-15 00:00:35 -0300

    Update README.py

--- Tareas/Tarea_02/README.py
+++ Tareas/Tarea_02/README.py
@@ -1,1 +1,8 @@
-https://colab.research.google.com/drive/1YAg9fsBCb1dkfL9ONndXFhWkaLWqEVlT?usp=sharing
+Para los Problemas 1 y 2, utilicé el mismo link. El primer recuadro de código busca generar los números del 1 al 100, mientras que el segundo busca generar todos los múltiplos de 3.
+https://colab.research.google.com/drive/1OGiUUYXeI3E262qKFWly4mwS9HjL4qQ2?usp=sharing
+
+En el Problema 3, la dificultad era generar una suma entre dos variables y determinar qué ocurre en cada resultado obtenido. Utilizamos el número 5 como referencia, al que se le debe sumar un número a elección. De este proceso se desprenden tres posibles resultados, que son los que deberían "imprimirse" al realizarse la suma.
+https://colab.research.google.com/drive/1QHraY9DFEIWKDkRFBVChRdcbACzs2hSo?usp=sharing
+
+En el Problema 4, la dificultad era generar aún más variables, una relación entre ellas y determinar qué ocurre en cada resultado obtenido. Utilizamos tres notas con ponderaciones distintas y buscamos obtener el promedio de ellas. De este proceso se desprenden dos posibles resultados, que son los que deberían "imprimirse" al realizar el proceso, "APROBASTE!" o "REPROBASTE".
+https://colab.research.google.com/drive/1E3CRbkLcjx83yiYGzZ89VgXZm59bhhw8?usp=sharing
